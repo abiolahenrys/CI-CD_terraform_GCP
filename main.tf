6295c6a85fcee92d099a15c41362e4ac4bf06e0b
abiolahenrys <abiolahenrys@gmail.com>
2024-10-29 17:49:33 +0000

    last commit

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "google_storage_bucket" "CI-CD_Pipline" {
   name          = "tt-github-pipeline"
   location      = "US"
+  project       = "tf-Pl-001"
   force_destroy = true
 
   public_access_prevention = "enforced"
